25a3da8a0050b8fd663e282e5a6a653a65811061
Congiuntivo <simone.decarli@studenti.unitn.it>
2023-11-09 12:22:04 +0100

    Splitted main into run and train

--- model-bp/src/run.cpp
+++ model-bp/src/run.cpp
@@ -1,99 +1,14 @@
 #include <iostream>
 
 #include "tiny_dnn/tiny_dnn.h"
+#include "../model-bp/src/train.cpp"
 
 /*
 
-FATTI:
-- aggiungere softmax
-- aggiungere loss cross entropy (output come probabilità, non come codifica)
-
 DA FARE:
-- separare in run.cpp e model.cpp
+- provare a togliere il padding
 */
 
-static void construct_net(tiny_dnn::network<tiny_dnn::sequential> &nn,
-                          tiny_dnn::core::backend_t backend_type)
-{
-
-  using fc = tiny_dnn::layers::fc;
-  using tanh = tiny_dnn::activation::tanh;
-  using softmax = tiny_dnn::activation::softmax;
-
-  // using tiny_dnn::core::connection_table;
-  // using padding = tiny_dnn::padding;
-
-  nn << fc(32 * 32, 200, true, backend_type) // F6, 32^2-in, 200-out
-     << tanh()
-     << fc(200, 10, true, backend_type) // F6, 200-in, 10-out
-     << softmax();
-}
-
-static void train_lenet(const std::string &data_dir_path,
-                        double learning_rate,
-                        const int n_train_epochs,
-                        const int n_minibatch,
-                        tiny_dnn::core::backend_t backend_type)
-{
-  // specify loss-function and learning strategy
-  tiny_dnn::network<tiny_dnn::sequential> nn;
-  tiny_dnn::adagrad optimizer;
-
-  construct_net(nn, backend_type);
-
-  std::cout << "load models..." << std::endl;
-
-  // load MNIST dataset
-  std::vector<tiny_dnn::label_t> train_labels, test_labels;
-  std::vector<tiny_dnn::vec_t> train_images, test_images;
-
-  tiny_dnn::parse_mnist_labels(data_dir_path + "/train-labels.idx1-ubyte",
-                               &train_labels);
-  tiny_dnn::parse_mnist_images(data_dir_path + "/train-images.idx3-ubyte",
-                               &train_images, -1.0, 1.0, 2, 2);
-  tiny_dnn::parse_mnist_labels(data_dir_path + "/t10k-labels.idx1-ubyte",
-                               &test_labels);
-  tiny_dnn::parse_mnist_images(data_dir_path + "/t10k-images.idx3-ubyte",
-                               &test_images, -1.0, 1.0, 2, 2);
-
-  std::cout << "start training" << std::endl;
-
-  tiny_dnn::progress_display disp(train_images.size());
-  tiny_dnn::timer t;
-
-  optimizer.alpha *=
-      std::min(tiny_dnn::float_t(4),
-               static_cast<tiny_dnn::float_t>(sqrt(n_minibatch) * learning_rate));
-
-  int epoch = 1;
-  // create callback
-  auto on_enumerate_epoch = [&]()
-  {
-    std::cout << "Epoch " << epoch << "/" << n_train_epochs << " finished. "
-              << t.elapsed() << "s elapsed." << std::endl;
-    ++epoch;
-    tiny_dnn::result res = nn.test(test_images, test_labels);
-    std::cout << res.num_success << "/" << res.num_total << std::endl;
-
-    disp.restart(train_images.size());
-    t.restart();
-  };
-
-  auto on_enumerate_minibatch = [&]()
-  { disp += n_minibatch; };
-
-  // training
-  nn.train<tiny_dnn::cross_entropy>(optimizer, train_images, train_labels, n_minibatch,
-                                    n_train_epochs, on_enumerate_minibatch,
-                                    on_enumerate_epoch);
-
-  std::cout << "end training." << std::endl;
-
-  // test and show results
-  nn.test(test_images, test_labels).print_detail(std::cout);
-  // save network model & trained weights
-  nn.save("bp-model");
-}
 
 static tiny_dnn::core::backend_t parse_backend_name(const std::string &name)
 {
@@ -208,7 +123,7 @@ int main(int argc, char **argv)
             << std::endl;
   try
   {
-    train_lenet(data_path, learning_rate, epochs, minibatch_size, backend_type);
+    train(data_path, learning_rate, epochs, minibatch_size, backend_type);
   }
   catch (tiny_dnn::nn_error &err)
   {
