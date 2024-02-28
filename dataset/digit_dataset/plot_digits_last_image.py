7cfc184e41064ca348b0244ec5f9f36218d3b5cb
Congiuntivo <simone.decarli@studenti.unitn.it>
2024-02-28 12:55:38 +0100

    Added digit dataset conversion

---
+++ dataset/digit_dataset/plot_digits_last_image.py
@@ -0,0 +1,42 @@
+"""
+=========================================================
+The Digit Dataset
+=========================================================
+
+This dataset is made up of 1797 8x8 images. Each image,
+like the one shown below, is of a hand-written digit.
+In order to utilize an 8x8 figure like this, we'd have to
+first transform it into a feature vector with length 64.
+
+See `here
+<https://archive.ics.uci.edu/ml/datasets/Pen-Based+Recognition+of+Handwritten+Digits>`_
+for more information about this dataset.
+
+"""
+
+# Code source: Gaël Varoquaux
+# Modified for documentation by Jaques Grobler
+# License: BSD 3 clause
+
+# import matplotlib.pyplot as plt
+
+from sklearn import datasets
+
+# Load the digits dataset
+(digits, targets) = datasets.load_digits(return_X_y=True)
+
+with open("digits.txt", "w") as f:
+    for (digit, target) in zip(digits, targets):
+        # Convert the target into one-hot encoding
+        one_hot = [0] * 10
+        one_hot[target] = 1
+        # Map digit to 0-1 range
+        digit /= 16
+        # Write to the file space separated features and one-hot encoding
+        f.write(" ".join(map(str, digit)) + " " + " ".join(map(str, one_hot)) + "\n")
+
+# Display the last digit
+# plt.figure(1, figsize=(3, 3))
+# last_digit_matrix = digits[-1].reshape(8, 8)
+# plt.imshow(last_digit_matrix, cmap=plt.cm.gray_r, interpolation="nearest")
+# plt.show()
