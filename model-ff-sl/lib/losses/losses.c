f6002e76977a3a3f411402a3fe6aaffdde27542c
Congiuntivo <simone.decarli@studenti.unitn.it>
2024-04-11 14:43:43 +0200

    Update losses documentation

--- model-ff-sl/lib/losses/losses.c
+++ model-ff-sl/lib/losses/losses.c
@@ -1,25 +1,60 @@
-#include <losses/losses.h>
+/**
+ * @file losses.c
+ * @brief This file contains the implementation of various loss functions used in neural networks.
+ */
 
+#include <losses/losses.h>
 #include <math.h>
 
+// Static function declarations.
 static double stable_sigmoid(double x);
 static double softplus(double x, double betha);
 
+/**
+ * @brief Calculates the feedforward loss for a given positive and negative gradient and a threshold.
+ *
+ * @param g_pos The positive gradient.
+ * @param g_neg The negative gradient.
+ * @param threshold The threshold value.
+ * @return The feedforward loss.
+ */
 double ff_loss(const double g_pos, const double g_neg, const double threshold)
 {
     return softplus(-g_pos + threshold, 1.0) + softplus(g_neg - threshold, 1.0);
 }
 
+/**
+ * @brief Calculates the partial derivative of the feedforward loss with respect to the positive gradient.
+ *
+ * @param g_pos The positive gradient.
+ * @param g_neg The negative gradient.
+ * @param threshold The threshold value.
+ * @return The partial derivative of the feedforward loss with respect to the positive gradient.
+ */
 double ff_pdloss_pos(const double g_pos, const double g_neg, const double threshold)
 {
     return -stable_sigmoid(threshold - g_pos);
 }
 
+/**
+ * @brief Calculates the partial derivative of the feedforward loss with respect to the negative gradient.
+ *
+ * @param g_pos The positive gradient.
+ * @param g_neg The negative gradient.
+ * @param threshold The threshold value.
+ * @return The partial derivative of the feedforward loss with respect to the negative gradient.
+ */
 double ff_pdloss_neg(const double g_pos, const double g_neg, const double threshold)
 {
     return stable_sigmoid(g_neg - threshold);
 }
 
+/**
+ * @brief Calculates the stable sigmoid function for a given input.
+ *
+ * @param x The input value.
+ * @return The sigmoid value.
+ */
 static double stable_sigmoid(double x)
 {
     if (x >= 0)
@@ -31,21 +66,52 @@ static double stable_sigmoid(double x)
     }
 }
 
+/**
+ * @brief Calculates the softplus function for a given input and beta value.
+ *
+ * @param x The input value.
+ * @param betha The beta value.
+ * @return The softplus value.
+ */
 static double softplus(double x, double betha)
 {
     return log(1.0 + exp(x * betha)) / betha;
 }
 
+/**
+ * @brief Calculates the SymBa loss for a given positive and negative gradient and a threshold.
+ *
+ * @param g_pos The positive gradient.
+ * @param g_neg The negative gradient.
+ * @param threshold The threshold value.
+ * @return The SymBa loss.
+ */
 double symba_loss(const double g_pos, const double g_neg, const double threshold)
 {
     return softplus(threshold * (g_pos - g_neg), 0.1);
 }
 
+/**
+ * @brief Calculates the partial derivative of the SymBa loss with respect to the positive gradient.
+ *
+ * @param g_pos The positive gradient.
+ * @param g_neg The negative gradient.
+ * @param threshold The threshold value.
+ * @return The partial derivative of the SymBa loss with respect to the positive gradient.
+ */
 double symba_pdloss_pos(const double g_pos, const double g_neg, const double threshold)
 {
     return threshold * stable_sigmoid(-threshold * (g_pos - g_neg));
 }
 
+/**
+ * @brief Calculates the partial derivative of the SymBa loss with respect to the negative gradient.
+ *
+ * @param g_pos The positive gradient.
+ * @param g_neg The negative gradient.
+ * @param threshold The threshold value.
+ * @return The partial derivative of the SymBa loss with respect to the negative gradient.
+ */
 double symba_pdloss_neg(const double g_pos, const double g_neg, const double threshold)
 {
     return -threshold * stable_sigmoid(-threshold * (g_pos - g_neg));
--- model-ff-sl/lib/losses/losses.h
+++ model-ff-sl/lib/losses/losses.h
@@ -1,7 +1,18 @@
+/**
+ * @file losses.h
+ * @brief This file contains the declarations for loss functions used in neural networks.
+ *
+ * This file provides the declarations for various loss functions that can be used in neural networks.
+ * Loss functions are used to measure the discrepancy between the predicted output and the actual output.
+ * These functions are commonly used in training neural networks to optimize the model's performance.
+ *
+ * @note This file is part of the lightweight-neural-nets library.
+ */
 #pragma once
 
-
-
+/**
+ * @brief Struct representing a Loss function and its partial derivatives.
+ */
 typedef struct
 {
     // Loss function.
@@ -12,17 +23,74 @@ typedef struct
     double (*pdloss_neg)(const double, const double, const double);
 } Loss;
 
-
-// FF original loss function.
+/**
+ * @brief FF original loss function.
+ *
+ * @param g_pos The positive gradient.
+ * @param g_neg The negative gradient.
+ * @param threshold The threshold value.
+ * @return The loss value.
+ */
 double ff_loss(const double g_pos, const double g_neg, const double threshold);
+
+/**
+ * @brief Partial derivative of the FF loss function for the positive pass.
+ *
+ * @param g_pos The positive gradient.
+ * @param g_neg The negative gradient.
+ * @param threshold The threshold value.
+ * @return The partial derivative of the loss function for the positive pass.
+ */
 double ff_pdloss_pos(const double g_pos, const double g_neg, const double threshold);
+
+/**
+ * @brief Partial derivative of the FF loss function for the negative pass.
+ *
+ * @param g_pos The positive gradient.
+ * @param g_neg The negative gradient.
+ * @param threshold The threshold value.
+ * @return The partial derivative of the loss function for the negative pass.
+ */
 double ff_pdloss_neg(const double g_pos, const double g_neg, const double threshold);
 
-// SymBa loss function.
+/**
+ * @brief SymBa loss function.
+ *
+ * @param g_pos The positive gradient.
+ * @param g_neg The negative gradient.
+ * @param threshold The threshold value.
+ * @return The loss value.
+ */
 double symba_loss(const double g_pos, const double g_neg, const double threshold);
+
+/**
+ * @brief Partial derivative of the SymBa loss function for the positive pass.
+ *
+ * @param g_pos The positive gradient.
+ * @param g_neg The negative gradient.
+ * @param threshold The threshold value.
+ * @return The partial derivative of the loss function for the positive pass.
+ */
 double symba_pdloss_pos(const double g_pos, const double g_neg, const double threshold);
+
+/**
+ * @brief Partial derivative of the SymBa loss function for the negative pass.
+ *
+ * @param g_pos The positive gradient.
+ * @param g_neg The negative gradient.
+ * @param threshold The threshold value.
+ * @return The partial derivative of the loss function for the negative pass.
+ */
 double symba_pdloss_neg(const double g_pos, const double g_neg, const double threshold);
 
-// Macros for on-demand Loss struct instantiation
-#define LOSS_FF (Loss){ff_loss, ff_pdloss_pos, ff_pdloss_neg}
-#define LOSS_SYMBA (Loss){symba_loss, symba_pdloss_pos, symba_pdloss_neg}
+/**
+ * @brief Original Forward Forward loss function.
+ */
+#define LOSS_FF \
+    (Loss) { ff_loss, ff_pdloss_pos, ff_pdloss_neg }
+
+/**
+ * @brief SymBa loss function.
+ */
+#define LOSS_SYMBA \
+    (Loss) { symba_loss, symba_pdloss_pos, symba_pdloss_neg }
