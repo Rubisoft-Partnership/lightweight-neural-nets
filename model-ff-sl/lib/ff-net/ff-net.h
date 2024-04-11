7679a92d7161234c02f54e1a0145bad8e29584fa
Congiuntivo <simone.decarli@studenti.unitn.it>
2024-04-11 11:05:18 +0200

    Refactor FFNet header file

--- model-ff-sl/lib/ff-net/ff-net.h
+++ model-ff-sl/lib/ff-net/ff-net.h
@@ -1,13 +1,11 @@
 #pragma once
 
-#include <stdio.h>
-
 #include <ff-cell/ff-cell.h>
 #include <losses/losses.h>
 
 #define MAX_LAYERS_NUM 64
 
-
+// FFNet struct that contains multiple Tinn objects.
 typedef struct
 {
     Tinn layers[MAX_LAYERS_NUM];
@@ -15,14 +13,13 @@ typedef struct
     Loss loss_suite;
 } FFNet;
 
-double train_ff_net(const FFNet ffnet, const double *const pos, const double *const neg, double rate);
+// Builds a FFNet by creating multiple Tinn objects.
 FFNet new_ff_net(const int *layer_sizes, int num_layers, double (*act)(double), double (*pdact)(double), const double treshold, Loss loss_suite);
+// Frees the memory of a FFNet.
 void free_ff_net(FFNet ffnet);
+// Trains a FFNet by training each cell.
+double train_ff_net(const FFNet ffnet, const double *const pos, const double *const neg, double rate);
+// Inference function for FFNet.
 int predict_ff_net(const FFNet ffnet, const double *in, const int num_classes, const int insize);
-
-// Activation function.
-
-double relu(const double a);
-double pdrelu(const double a);
 
 
