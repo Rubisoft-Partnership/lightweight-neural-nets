377ee9cec28e2e96f903881e3f74893b7cfb5fe7
Congiuntivo <simone.decarli@studenti.unitn.it>
2024-04-16 14:23:38 +0200

    Refactor memory allocation in adam.c for better initialization

--- model-ff-sl/lib/adam/adam.c
+++ model-ff-sl/lib/adam/adam.c
@@ -23,13 +23,9 @@ Adam adam_create(const double beta1, const double beta2, const int size)
     // Initialize time step to 1
     adam.t = 1;
     
-    // Allocate memory for m and v
-    adam.m = malloc(size * sizeof(double));
-    adam.v = malloc(size * sizeof(double));
-
-    // Initialize m and v to 0
-    memset(adam.m, 0, size * sizeof(double));
-    memset(adam.v, 0, size * sizeof(double));
+    // Allocate memory for m and v 0-initialized
+    adam.m = (double*)calloc(size, sizeof(double));
+    adam.v = (double*)calloc(size, sizeof(double));
 
     return adam;
 }
